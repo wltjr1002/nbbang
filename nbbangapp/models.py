f2c3032712eef947c60cf343edb2ddf2a7b0dccc
김지석 <wltjr1002@gmail.com>
2017-05-13 18:32:26 +0900

    model complete

--- nbbangapp/models.py
+++ nbbangapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+
+class Room(models.Model):
+    group = models.ForeignKey(Group)
+
+    ownerName = models.ForeignKey(Person)
+    participants = models.ManyToManyField(Person)
+
+    moneyTotal = models.IntegerField()
+    moneyRemain = models.IntegerField()
+
+    created_date = models.DateTimeField(
+            default=timezone.now)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+class Group(models.Model):
+    people = models.ManyToManyField(Person)
+
+class Person(models.Model):
+    name = models.CharField(max_length = 10)
+    bank = models.CharField(max_length = 5)
+    account = models.CharField(max_length = 20)
+
 # Create your models here.
